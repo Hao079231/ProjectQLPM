fa367c2235e291e0cb475b3529ebc1adcdaf8924
Nguyen Hong Ha <157662167+Ha1910@users.noreply.github.com>
2025-04-08 13:24:45 +0700

    up fodel

--- BookShopWeb-main/src/main/java/com/bookshopweb/utils/ConstantUtils.java
+++ BookShopWeb-main/src/main/java/com/bookshopweb/utils/ConstantUtils.java
@@ -1,10 +1,10 @@
 package com.bookshopweb.utils;
 
 public interface ConstantUtils {
-    int DB_PORT = 3306;
+    int DB_PORT = 3308;
     String SERVER_NAME = "localhost";
     String DB_NAME = "bookshopdb";
     String DB_USERNAME = "root";
-    String DB_PASSWORD = "1234567";
+    String DB_PASSWORD = "123456789";
     String IMAGE_PATH = "../img";
 }
